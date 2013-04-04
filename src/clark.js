29eaba03cc6548c4e81bab1fae6f299639b0542d
Jack Lawson <ajacksified@gmail.com>
2013-04-04 16:57:58 -0700

    updated docs

--- src/clark.js
+++ src/clark.js
@@ -1,24 +1,32 @@
 (function(root) {
+  // Define the various steps; we'll use 7 fairly safe ticks.
   var ticks = ['▁', '▂', '▃', '▄', '▅', '▆', '▇'],
       data;
 
+  // Build the actual clark function, which takes an array of numbers and
+  // outputs back the graph as a string.
   var clark = function(data) {
+
+    // Find the min and max and normalize into steps based on the number of 
+    // ticks.
     var min = Math.min.apply(Math, data),
         max = (Math.max.apply(Math, data) - min) / (ticks.length - 1),
-        currentTick;
+        results = [];
 
-    var i, len, results = [];
-
-    for (i = 0, len = data.length; i < len; i++) {
-      currentTick = data[i];
-      results.push(ticks[(currentTick - min) / max >> 0]);
+    for (var i = 0, len = data.length; i < len; i++) {
+      // Pick the correct tick based on the min-shifted value.
+      results.push(ticks[parseInt((data[i] - min) / max)]);
     }
 
+    // Smush together the array of tick asciis.
     return results.join('');
   };
 
-  /* expose it to the world */
 
+  // Expose the module in whatever the most appropriate way probably is based
+  // on the global environment.
+
+  // Node.js
   if((typeof module !== "undefined") && module.exports){
     module.exports = clark;
   } else if((typeof define === 'function') && define.amd) {
